58fa273658b020159f0ac22dbaa7562e740b4fc0
gerardo jerez <gerardo.jerez@hotmail.com>
2022-05-27 08:24:16 -0700

    listo?

---
+++ app/Http/Controllers/Api/CapturasController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Datos;
+
+class CapturasController extends Controller
+{
+    public function store(Request $request) {
+        $respuesta = array();
+        $respuesta['exito'] = false;
+
+        $nuevaCaptura = new Datos();
+
+        $nuevaCaptura -> marca = $request -> input('marca');
+        $nuevaCaptura -> color = $request -> input('color');
+        $nuevaCaptura -> tipo = $request -> input('tipo');
+        $nuevaCaptura -> year = $request -> input('year');
+        $nuevaCaptura -> nombre = $request -> input('nombre');
+        $nuevaCaptura -> apellido = $request -> input('apellido');
+        $nuevaCaptura -> direccion = $request -> input('direccion');
+        $nuevaCaptura -> pais = $request -> input('pais');
+        $nuevaCaptura -> estado = $request -> input('estado');
+        $nuevaCaptura -> codigo_postal = $request -> input('codigo_postal');
+        $nuevaCaptura -> correo_electronico = $request -> input('correo_electronico');
+        $nuevaCaptura -> celular = $request -> input('celular');
+
+        $nuevaCaptura -> save();
+        /*if($nuevaCaptura -> save()){
+            $respuesta['exito'] = true;
+        }*/
+
+        return $respuesta;
+
+    }
+    
+
+}
---
+++ formulario.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-05-2022 a las 17:23:13
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `formulario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos`
+--
+
+CREATE TABLE `datos` (
+  `id` int(11) NOT NULL,
+  `marca` varchar(100) NOT NULL,
+  `color` varchar(100) NOT NULL,
+  `tipo` varchar(100) NOT NULL,
+  `year` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `estado` varchar(100) NOT NULL,
+  `codigo_postal` varchar(100) NOT NULL,
+  `correo_electronico` varchar(100) NOT NULL,
+  `celular` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `datos`
+--
+ALTER TABLE `datos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `datos`
+--
+ALTER TABLE `datos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
